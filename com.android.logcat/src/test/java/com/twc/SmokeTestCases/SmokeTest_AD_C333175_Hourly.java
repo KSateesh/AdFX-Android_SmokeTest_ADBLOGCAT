9afc326f40a89071007c6f105b216aa513dc6237
KSateesh <sateesh.kumar.contractor@weather.com>
2015-11-10 21:47:03 +0530

    Adding the Hourly test case
    Try to run in Samsung S6 device and let me know your comments

--- com.android.logcat/src/test/java/com/twc/SmokeTestCases/SmokeTest_AD_C333175_Hourly.java
+++ com.android.logcat/src/test/java/com/twc/SmokeTestCases/SmokeTest_AD_C333175_Hourly.java
@@ -23,83 +23,64 @@ import com.twc.General.app_Kill_Relaunch;
 import com.twc.driver.Driver;
 
 @SuppressWarnings("unused")
-public class SmokeTest_AD_C333175_Hourly extends Driver{
-
-
-	@SuppressWarnings("unused")
-	public void verify_adpresent_onextendedHourly_page() throws Exception{
-	
-		//app kill and relaunch the app
-				app_Kill_Relaunch.Kill_realaunch();
-
-
-				String originalContext = Ad.getContext();
-				Ad.context("NATIVE_APP");
-				
-				
-				MobileElement ele = (MobileElement) Ad.findElementById("com.weather.Weather:id/temperature");
-
-				WebDriverWait wait = new WebDriverWait(Ad, 4);
-				
-				wait.until(ExpectedConditions.visibilityOf(ele));
-
-		Dimension dimensions = Ad.manage().window().getSize();
-//		//System.out.println("dimensions :: "+dimensions);
-//     	int scrollStart =2300;
-//		int scrollEnd =50;
-//		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-//		//Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-	
-				
-				//Swipe.swipe();
-				
-		for (int i=0; i<dimensions.getHeight(); i++) {
-		
-			WebElement hourly=null;
-
-			try{
-				hourly = Ad.findElementById("com.weather.Weather:id/hourly_title_textview");
-			System.out.println("In for loop"+i);
-			}catch(Exception e){
-				//System.out.println(e);
-				//Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-			}
-			//Ad.findElementById("com.weather.Weather:id/hourly_title_textview").isDisplayed()
-			
-			if(hourly!=null && hourly.isDisplayed())
-			{ 
-				System.out.println("Hourly section is displayed and tapping on MORE button");
-
-				Ad.findElementById("com.weather.Weather:id/hourly_more").click();
-
-				MobileElement AdEle =(MobileElement) Ad.findElementById("com.weather.Weather:id/ad_view_holder");
-
-				WebDriverWait wait1 = new WebDriverWait(Ad, 4);
-				
-				wait1.until(ExpectedConditions.visibilityOf(AdEle));
-			
-				if(AdEle.isDisplayed())
-				{
-					
-					System.out.println("Ad is displayed on Hourly-More page");
-
-					Thread.sleep(6000);
-					// Clicking back button
-					Ad.findElement(By.id("android:id/home")).click();
-					
-
-				}break;
-
-			}else
-			{
-				System.out.println("Hourly section is not present and scrolling again");
-//				System.out.println("Scrolling "+i+" time");
-//				Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-				Swipe.swipe();
-		
-			}
-			
-			
-		}
-	}
+public class SmokeTest_AD_C333175_Hourly extends Driver {
+    
+    @SuppressWarnings("unused")
+    public void verify_adpresent_onextendedHourly_page() throws Exception {
+        
+        // app kill and relaunch the app
+        app_Kill_Relaunch.Kill_realaunch();
+        
+        String originalContext = Ad.getContext();
+        Ad.context("NATIVE_APP");
+        
+        //To get the dimensions of the screen
+        Dimension dimensions = Ad.manage().window().getSize();
+        //System.out.println("dimensions :: "+dimensions);
+        
+        System.out.println("Searching for Hourly section");
+        
+        for (int i = 0; i < dimensions.getHeight(); i++) {
+            
+            WebElement hourly = null;
+            
+            try {
+                hourly = Ad.findElementById("com.weather.Weather:id/hourly_title_textview");
+                
+            } catch (Exception e) {
+                // System.out.println(e);
+            }
+            
+            if (hourly!= null && hourly.isDisplayed()) {
+                
+                System.out.println("Hourly section is displayed and tapping on MORE button");
+                
+                Ad.findElementById("com.weather.Weather:id/hourly_more").click();
+                
+                MobileElement AdEle = (MobileElement) Ad.findElementById("com.weather.Weather:id/ad_view_holder");
+                
+                WebDriverWait wait1 = new WebDriverWait(Ad, 4);
+                
+                wait1.until(ExpectedConditions.visibilityOf(AdEle));
+                
+                if (AdEle.isDisplayed()) {
+                    
+                    System.out.println("Ad is displayed on Hourly-Extended page");
+                    
+                    Thread.sleep(2000);
+                    // Clicking back button
+                    Ad.findElement(By.id("android:id/home")).click();
+                    
+                }break;
+                
+            } else {
+                
+                System.out.println("Hourly section is not presented and scrolling down");
+                
+                Swipe.swipe();
+                
+            }
+            
+        }
+    }
 }
--- com.android.logcat/src/test/java/com/twc/SmokeTestCases/SmokeTest_AD_C333176_Map.java
+++ com.android.logcat/src/test/java/com/twc/SmokeTestCases/SmokeTest_AD_C333176_Map.java
@@ -8,74 +8,76 @@ import java.util.HashMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.twc.General.Swipe;
 import com.twc.General.app_Kill_Relaunch;
 import com.twc.driver.Driver;
 
 public class SmokeTest_AD_C333176_Map extends Driver{
-
-	@SuppressWarnings("unused")
-	public void verify_adpresent_onextendedMap_page() throws Exception
-	{
-		
-		//app kill and relaunch the app
-		app_Kill_Relaunch.Kill_realaunch();
-
-		String originalContext = Ad.getContext();
-		Ad.context("NATIVE_APP");
-
-		MobileElement ele = (MobileElement) Ad.findElementById("com.weather.Weather:id/temperature");
-
-		WebDriverWait wait = new WebDriverWait(Ad, 4);
-
-		wait.until(ExpectedConditions.visibilityOf(ele));
-
-		Dimension dimensions = Ad.manage().window().getSize();
-		// System.out.println("dimensions :: "+dimensions);
-		int scrollStart = 2300;
-		int scrollEnd = 50;
-		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-		Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-
-		for (int i=0; i<dimensions.getHeight(); i++) 
-		{
-			
-			if(Ad.findElementById("com.weather.Weather:id/hurricane_central_module_title").isDisplayed())
-			{
-				if(!Ad.findElementById("com.weather.Weather:id/hurricane_central_module_title").isDisplayed());
-				{
-				System.out.println("Map element is not present");
-				}
-				
-				System.out.println("Map page is displyaed and tapping on Map button");
-				
-				Ad.findElementById("com.weather.Weather:id/map_module_thumbnail").click();
-				
-				MobileElement AdEle =(MobileElement)Ad.findElementById("com.weather.Weather:id/ad_view_holder"); //csAd640 //com.weather.Weather:id/ad_view_holder
-
-				WebDriverWait wait1 = new WebDriverWait(Ad, 4);
-				wait1.until(ExpectedConditions.visibilityOf(AdEle));
-				
-				if(AdEle.isDisplayed())
-				{
-					Thread.sleep(6000);
-					System.out.println("Ad present on Extended Map page");
-					// Clicking back button
-					Ad.findElement(By.id("android:id/home")).click();
-					Thread.sleep(6000);
-				}break;
-
-			}else
-			{
-				System.out.println("Map element is not present");
-				Ad.swipe(0,scrollStart,0,scrollEnd,2000);
-				
-			}
-		}
-	}
-
+    
+    @SuppressWarnings("unused")
+    public void verify_adpresent_onextendedMap_page() throws Exception
+    {
+        
+        //app kill and relaunch the app
+        app_Kill_Relaunch.Kill_realaunch();
+        
+        String originalContext = Ad.getContext();
+        Ad.context("NATIVE_APP");
+        
+        //To get the dimensions of the screen
+        Dimension dimensions = Ad.manage().window().getSize();
+        // System.out.println("dimensions :: "+dimensions); //4
+        
+        System.out.println("Searching for Maps section");
+        
+        for (int i=0; i<dimensions.getHeight(); i++)
+        {
+            WebElement maps = null;
+            
+            try {
+                maps = Ad.findElementById("com.weather.Weather:id/hurricane_central_module_title");
+                
+            } catch (Exception e) {
+                // System.out.println(e);
+            }
+            
+            if(maps!=null&& maps.isDisplayed())
+            {
+                if(!maps.isDisplayed());
+                {
+                    System.out.println("Maps element is not present");
+                }
+                
+                System.out.println("Maps section is displyaed and tapping on Map button");
+                
+                Ad.findElementById("com.weather.Weather:id/map_module_thumbnail").click();
+                
+                MobileElement AdEle =(MobileElement)Ad.findElementById("com.weather.Weather:id/ad_view_holder"); //csAd640 //com.weather.Weather:id/ad_view_holder
+                
+                WebDriverWait wait1 = new WebDriverWait(Ad, 4);
+                wait1.until(ExpectedConditions.visibilityOf(AdEle));
+                
+                if(AdEle.isDisplayed())
+                {
+                    Thread.sleep(6000);
+                    System.out.println("Ad present on Extended Map page");
+                    // Clicking back button
+                    Ad.findElement(By.id("android:id/home")).click();
+                    Thread.sleep(6000);
+                }break;
+                
+            }else
+            {
+                System.out.println("Maps section is not presented and scrolling down");
+                
+                Swipe.swipe();
+                
+            }
+        }
+    }
+    
 }
